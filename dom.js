e27fdac08745c1740728e5b056d5545bddf5b38e
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-12-15 19:26:23 -0300

    Clase DOM: Texto y HTML

--- dom.js
+++ dom.js
@@ -110,24 +110,44 @@
 // $html.style.setProperty("--dark-color", "#000");
 // varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 // $body.style.setProperty("background-color", varDarkColor);
-const $card = document.querySelector(".card");
+// const $card = document.querySelector(".card");
 
-console.log($card);
-console.log($card.className);
-console.log($card.classList);
-console.log($card.classList.contains("rotate-45"));
-$card.classList.add("rotate-45");
-console.log($card.classList.contains("rotate-45"));
-console.log($card.className);
-console.log($card.classList);
-$card.classList.remove("rotate-45");
-console.log($card.classList.contains("rotate-45"));
-$card.classList.toggle("rotate-45");
-console.log($card.classList.contains("rotate-45"));
-$card.classList.toggle("rotate-45");
-console.log($card.classList.contains("rotate-45"));
-$card.classList.toggle("rotate-45");
-$card.classList.replace("rotate-45", "rotate-135");
-$card.classList.add("opacity-80", "sepia");
-$card.classList.remove("opacity-80", "sepia");
-$card.classList.toggle("opacity-80", "sepia");
+// console.log($card);
+// console.log($card.className);
+// console.log($card.classList);
+// console.log($card.classList.contains("rotate-45"));
+// $card.classList.add("rotate-45");
+// console.log($card.classList.contains("rotate-45"));
+// console.log($card.className);
+// console.log($card.classList);
+// $card.classList.remove("rotate-45");
+// console.log($card.classList.contains("rotate-45"));
+// $card.classList.toggle("rotate-45");
+// console.log($card.classList.contains("rotate-45"));
+// $card.classList.toggle("rotate-45");
+// console.log($card.classList.contains("rotate-45"));
+// $card.classList.toggle("rotate-45");
+// $card.classList.replace("rotate-45", "rotate-135");
+// $card.classList.add("opacity-80", "sepia");
+// $card.classList.remove("opacity-80", "sepia");
+// $card.classList.toggle("opacity-80", "sepia");
+
+// Clase 66 DOM: Texto y HTML
+const $whatIsDOM = document.getElementById("que-es");
+
+let text = `
+    <p>
+    El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML.
+    </p>
+    <p>
+    Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+    <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+    </p>
+`;
+
+//$whatIsDOM.innerText = text;
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+$whatIsDOM.outerHTML = text;
