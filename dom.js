d9aad0d8c171872884733bb0e0e5ebdd62b93d72
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-12-14 23:56:02 -0300

    Clase DOM: Nodos, Elementos y Selectores

--- dom.js
+++ dom.js
@@ -5,21 +5,36 @@
 
 //         hablar(texto);
 
-console.log("Elementos del Documento");
-console.log(window.document);
-console.log(document);
-console.log(document.head);
-console.log(document.body);
-console.log(document.documentElement);
-console.log(document.doctype);
-console.log(document.characterSet);
-console.log(document.title);
-console.log(document.links);
-console.log(document.images);
-console.log(document.forms);
-console.log(document.styleSheets);
-console.log(document.scripts);
-setTimeout(() => {
-    console.log(document.getSelection().toString());
-}, 3000);
-document.write("<h2>Hola mundo desde el DOM</h2>")
+// console.log("Elementos del Documento");
+// console.log(window.document);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.characterSet);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
+// setTimeout(() => {
+//     console.log(document.getSelection().toString());
+// }, 3000);
+// document.write("<h2>Hola mundo desde el DOM</h2>")
+
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+console.log(document.querySelector("#menu"));
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach((link) => console.log(link));
+console.log(document.querySelector(".card"));
+console.log(document.querySelectorAll(".card"));
+console.log(document.querySelectorAll(".card")[2]);
+console.log(document.querySelectorAll("#menu li"));
+console.log(document.querySelector("#menu li"));
