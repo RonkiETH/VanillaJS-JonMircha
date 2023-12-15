d0bacfee785ad1d98ec885173daa919e63a32938
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-12-15 18:00:06 -0300

    Clase DOM: Atributos y Data-Attributes

--- dom.js
+++ dom.js
@@ -24,17 +24,49 @@
 // }, 3000);
 // document.write("<h2>Hola mundo desde el DOM</h2>")
 
-console.log(document.getElementsByTagName("li"));
-console.log(document.getElementsByClassName("card"));
-console.log(document.getElementsByName("nombre"));
-console.log(document.getElementById("menu"));
-console.log(document.querySelector("#menu"));
-console.log(document.querySelector("a"));
-console.log(document.querySelectorAll("a"));
-console.log(document.querySelectorAll("a").length);
-document.querySelectorAll("a").forEach((link) => console.log(link));
-console.log(document.querySelector(".card"));
-console.log(document.querySelectorAll(".card"));
-console.log(document.querySelectorAll(".card")[2]);
-console.log(document.querySelectorAll("#menu li"));
-console.log(document.querySelector("#menu li"));
+//Clase 62 DOM
+
+// console.log(document.getElementsByTagName("li"));
+// console.log(document.getElementsByClassName("card"));
+// console.log(document.getElementsByName("nombre"));
+// console.log(document.getElementById("menu"));
+// console.log(document.querySelector("#menu"));
+// console.log(document.querySelector("a"));
+// console.log(document.querySelectorAll("a"));
+// console.log(document.querySelectorAll("a").length);
+// document.querySelectorAll("a").forEach((link) => console.log(link));
+// console.log(document.querySelector(".card"));
+// console.log(document.querySelectorAll(".card"));
+// console.log(document.querySelectorAll(".card")[2]);
+// console.log(document.querySelectorAll("#menu li"));
+// console.log(document.querySelector("#menu li"));
+
+//Clase 63 DOM
+
+console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = "en";
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang", "es-MX");
+
+// Cuando una variable es un elemento del dom, declararla con $ adelante, es una buena práctica.
+const $linkDOM = document.querySelector(".link-dom");
+
+$linkDOM.setAttribute("target", "_blank");
+$linkDOM.setAttribute("rel", "noopener");
+$linkDOM.setAttribute("href", "https://youtube.com/jonmircha");
+console.log($linkDOM.hasAttribute("rel"));
+$linkDOM.removeAttribute("rel");
+console.log($linkDOM.hasAttribute("rel"));
+
+//Data-Attributes
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento");
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Suscríbete a mi canal y comparte";
+console.log($linkDOM.dataset.description);
