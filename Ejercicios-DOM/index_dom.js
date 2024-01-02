4a362ad898c0661c86633137db8d176a2e73826f
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2024-01-02 18:21:00 -0300

    Ejercicios DOM - Cuenta Regresiva countdown

--- Ejercicios-DOM/index_dom.js
+++ Ejercicios-DOM/index_dom.js
@@ -1,11 +1,13 @@
 import hamburgerMenu from "./menu_hamburgesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { shortcuts, moveBall } from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-    alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma")
+    alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+    countdown("countdown", "Jan 02, 2024 18:21:00", "Feliz cumpleaños Ronki");
 })
 
 document.addEventListener("keydown", (e) => {
