96f688c6afdcdb610be9d5d58a8819d3894f5a02
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2024-01-19 02:16:03 -0300

    Validaciones de Formulario

--- Ejercicios-DOM/index_dom.js
+++ Ejercicios-DOM/index_dom.js
@@ -5,6 +5,7 @@ import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
+import contactFormValidations from "./validaciones_formulario.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
@@ -23,6 +24,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
         "(min-width: 1024px)", 
         `<a href="https://maps.app.goo.gl/WQuhHgHxvwiFYdJ98" target="_blank" rel="noopener">Ver mapa</a>`, 
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52521.10854449382!2d-58.596710465917546!3d-34.64032328970931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc93a3ba895ab%3A0xf7aedd3fb3853a84!2sMcDonald&#39;s!5e0!3m2!1ses-419!2sar!4v1704237485165!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
+
+    contactFormValidations();  
         
 })
 
